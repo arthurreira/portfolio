19692e0812d9394f05e369fc75a1ad1a80ccee6c
Arthur Ferreira <arthur.ferreiramiran@gmail.com>
2026-04-28 21:29:41 +0300

    feat: add gradient color to hero heading

--- apps/web/components/atoms/hero/heroText/heroText.tsx
+++ apps/web/components/atoms/hero/heroText/heroText.tsx
@@ -9,8 +9,8 @@ export default function HeroText({
   return (
       <div className="space-y-4">
 
-            <h1 className="relative font-semibold max-w-max justify-center pb-2 font-display text-3xl sm:text-4xl md:text-5xl text-foreground after:absolute after:bottom-0 after:left-0 after:h-0.5 after:rounded-lg after:bg-foreground after:w-4">
-                {heading}
+            <h1 className="relative font-semibold max-w-max justify-center pb-2 font-display text-3xl sm:text-4xl md:text-5xl bg-gradient-to-r from-foreground to-primary bg-clip-text text-transparent after:absolute after:bottom-0 after:left-0 after:h-0.5 after:rounded-lg after:bg-primary after:w-4">
+                  {heading}
             </h1>
             <p className="font-display font-semibold  text-foreground">
                 {subtitle}
